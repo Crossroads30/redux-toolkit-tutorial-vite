75ace153aea353795e808fecedf031e23be2d137
crossroads30 <crossroads30@mail.ru>
2024-01-12 23:18:32 +0300

    feat: add modal reducer

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from './features/cart/cartSlice'
+import modalReducer from './features/modal/modalSlice'
 
 export const store = configureStore({
 	reducer: {
-    cart: cartReducer,// (cartSlice.reducer)
-  },
+		cart: cartReducer, // (cartSlice.reducer)
+		modal: modalReducer, // (modalSlice.reducer)
+	},
 })
