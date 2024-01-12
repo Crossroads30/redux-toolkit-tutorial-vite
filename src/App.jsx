a74aca58ebcec0947f078fc1da69dd11e68f57f8
crossroads30 <crossroads30@mail.ru>
2024-01-13 00:25:57 +0300

    feat: add data from fetching, add loading cond

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,29 @@ import { useSelector, useDispatch } from 'react-redux'
 import CartContainer from './components/CartContainer'
 import Navbar from './components/Navbar'
 import { useEffect } from 'react'
-import { calculatesTotals } from './features/cart/cartSlice'
+import { calculatesTotals, getCartItems } from './features/cart/cartSlice'
 import Modal from './components/Modal'
 
 function App() {
-	const { cartItems } = useSelector(store => store.cart)
+	const { cartItems, isLoading } = useSelector(store => store.cart)
 	const { isOpen } = useSelector(store => store.modal)
 	const dispatch = useDispatch()
 
 	useEffect(() => {
 		dispatch(calculatesTotals())
 	}, [cartItems])
+
+	useEffect(() => {
+		dispatch(getCartItems())
+	}, [])
+
+	if (isLoading) {
+		return (
+			<div className='loading'>
+				<h1>Loading...</h1>
+			</div>
+		)
+	}
 
 	return (
 		<main>
