b2b75fdfba6b0dbee5b8e8c8335f63fcf80624e8
crossroads30 <crossroads30@mail.ru>
2024-01-12 22:24:35 +0300

    feat: add useSelector, useDispatch, calculate

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { useSelector, useDispatch } from 'react-redux'
 import CartContainer from './components/CartContainer'
 import Navbar from './components/Navbar'
-
+import { useEffect } from 'react'
+import {calculatesTotals} from './features/cart/cartSlice'
 
 function App() {
+	const { cartItems } = useSelector(store => store.cart)
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		dispatch(calculatesTotals())
+	}, [cartItems])
+
 	return (
 		<main>
 			<Navbar />
-      <CartContainer />
+			<CartContainer />
 		</main>
 	)
 }
