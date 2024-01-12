4d4f92aa06502bcbe79f2fe6098d809af351b8f6
crossroads30 <crossroads30@mail.ru>
2024-01-13 00:24:49 +0300

    feat: add extra reducers with builder callback notation

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,12 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit'
-import cartItems from '../../cartItems'
+import {
+	buildCreateSlice,
+	createAsyncThunk,
+	createSlice,
+} from '@reduxjs/toolkit'
+// import cartItems from '../../cartItems'
+
+const url = 'https://course-api.com/react-useReducer-cart-project'
 
 const initialState = {
-	cartItems: cartItems,
+	cartItems: [],
 	amount: 0,
 	total: 0,
 	isLoading: true,
 }
+
+export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
+	// first param: sliceType/action itself, second: callback function
+	return fetch(url)
+		.then(response => response.json())
+		.catch(error => console.log(error))
+})
 
 const cartSlice = createSlice({
 	name: 'cart',
@@ -59,6 +72,22 @@ const cartSlice = createSlice({
 			state.amount = amount
 			state.total = total
 		},
+	},
+	extraReducers: builder => {
+		// three lifecycle actions(pending, fulfilled, rejected):
+		builder
+			.addCase(getCartItems.pending, state => {
+				state.isLoading = true
+			})
+			.addCase(getCartItems.fulfilled, (state, action) => {
+				console.log(action)
+				state.isLoading = false
+				state.cartItems = action.payload
+			})
+			.addCase(getCartItems.rejected, (state, action) => {
+				console.log(action)
+				state.isLoading = false
+			})
 	},
 })
 
