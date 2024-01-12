f6140113ebe515462d9ee9fbb38b47535a7c24c2
crossroads30 <crossroads30@mail.ru>
2024-01-12 21:22:04 +0300

    feat: actions for increase/decrease amount, & common action for inc & dec

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -25,11 +25,39 @@ const cartSlice = createSlice({
 		removeItem: (state, action) => {
 			const itemId = action.payload
 			state.cartItems = state.cartItems.filter(item => item.id !== itemId)
-		}
+		},
+
+		// increaseAmount: (state, { payload }) => {
+		// 	const cartItem = state.cartItems.find(item => item.id === payload.id)
+		// 	cartItem.amount = cartItem.amount + 1
+		// },
+
+		// decreaseAmount: (state, { payload }) => {
+		// 	const cartItem = state.cartItems.find(item => item.id === payload.id)
+		// 	cartItem.amount = cartItem.amount - 1
+		// },
+
+		toggleAmount: (state, { payload }) => {
+			state.cartItems.map(item => {
+				if (item.id === payload.id) {
+					if (payload.type === 'increase') {
+						item.amount = item.amount + 1
+					} else if (payload.type === 'decrease') {
+						item.amount = item.amount - 1
+					}
+				}
+			})
+		},
 	},
 })
 
 // console.log(cartSlice)
-export const { clearCart, removeItem } = cartSlice.actions
+export const {
+	clearCart,
+	removeItem,
+	toggleAmount,
+	increaseAmount,
+	decreaseAmount,
+} = cartSlice.actions
 
 export default cartSlice.reducer // it will be cartReducer in 'store.js'
