742eb68091734ebf6ddc16353ab3e370d9d9fbed
crossroads30 <crossroads30@mail.ru>
2024-01-12 14:51:06 +0300

    feat: add cartContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import CartContainer from './components/CartContainer'
 import Navbar from './components/Navbar'
+
 
 function App() {
 	return (
 		<main>
 			<Navbar />
+      <CartContainer />
 		</main>
 	)
 }
