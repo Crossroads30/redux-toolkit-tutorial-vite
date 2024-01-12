f6dd2c7d8eb1a5917b321ae6037e54b68fdcfc07
crossroads30 <crossroads30@mail.ru>
2024-01-12 15:27:30 +0300

    feat: add 'return' issue comment

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -8,19 +8,22 @@ const initialState = {
 	isLoading: true,
 }
 
-
 const cartSlice = createSlice({
 	name: 'cart',
 	initialState,
 	reducers: {
-		clearCart: (state) => {
+		clearCart: state => {
 			state.cartItems = []
-		}
-	}
+			// whatever we return from action will become a new state,
+			// so if we omit rest props, they will be gone
+			// return {
+			// cartItems: [],
+			//   }
+		},
+	},
 })
 
 // console.log(cartSlice)
 export const { clearCart } = cartSlice.actions
 
-export default cartSlice.reducer // it will be cartReducer in 'store.js' 
-
+export default cartSlice.reducer // it will be cartReducer in 'store.js'
